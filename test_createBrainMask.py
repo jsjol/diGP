222d261f2a451dd590109bbd39c6cec775b74526
jsjol <sjolund.jens@gmail.com>
2016-12-14 16:17:24 +0100

    Reactions to code review, removed realistic test, added save functionality
    Added description. Removed definition of parameter values to what was
    anyway the default. Moved the test that brain volume is realistic to an
    integration test instead. Added save functionality (shouldn't have been
    part of this commit, sorry).

--- test_createBrainMask.py
+++ test_createBrainMask.py
@@ -2,37 +2,52 @@
 
 
 import unittest
-from os.path import join, expanduser, isdir
+from os import remove
+from os.path import join, expanduser
 import numpy as np
-from dipy.data.fetcher import fetch_scil_b0, read_siemens_scil_b0
+import nibabel as nib
 from createBrainMask import createBrainMaskFromb0Data
 
 
 class TestCreateBrainMask(unittest.TestCase):
+    dataDirectory = None
+    b0Data = np.array([0])
+    affine = np.eye(4)
+
+    def setUp(self):
+        homeDirectory = expanduser('~')
+        self.dataDirectory = join(homeDirectory, '.dipy')
+        self.b0Data = np.reshape(np.arange(3375.0), (15, 15, 15))
+        self.affine = np.array([[1., 0., 0., 1.],
+                                [0., 2., 0., 2.],
+                                [0., 0., 3., 3.],
+                                [0., 0., 0., 1.]])
+
+    def tearDown(self):
+        try:
+            remove(join(self.dataDirectory, 'brainMask.nii.gz'))
+        except OSError:
+            pass
 
     def test_createBrainMaskFromb0Data(self):
-        homeDirectory = expanduser('~')
-        dataDirectory = join(homeDirectory, '.dipy',
-                             'datasets_multi-site_all_companies')
-
-        if not isdir(dataDirectory):
-            fetch_scil_b0()
-
-        b0Img = read_siemens_scil_b0()
-        b0Data = np.squeeze(b0Img.get_data())
-
-        brainMask = createBrainMaskFromb0Data(b0Data)
-        self.assertTrue(np.array_equal(brainMask.shape, b0Data.shape))
+        brainMask = createBrainMaskFromb0Data(self.b0Data)
+        self.assertTrue(np.array_equal(brainMask.shape, self.b0Data.shape))
         # Want to test that brainMask is boolean, but couldn't find a built-in
         # function
         dataIsTrueOrFalse = np.logical_or(brainMask == 0, brainMask == 1)
         self.assertTrue(np.array_equal(dataIsTrueOrFalse,
                                        np.ones(brainMask.shape)))
-        self.assertTrue(np.all(b0Data[brainMask == 1] > 0))
-        voxelSizes = b0Img.header.get_zooms()[:3]
-        voxelVolumeInCubicCentimeters = 1e-3*np.prod(voxelSizes)
-        brainVolume = voxelVolumeInCubicCentimeters*np.sum(brainMask)
-        self.assertTrue(brainVolume > 500 and brainVolume < 1500)
+        self.assertTrue(np.all(self.b0Data[brainMask == 1] > 0))
+
+    def test_saveAndLoadBrainMask(self):
+        brainMask = createBrainMaskFromb0Data(self.b0Data,
+                                              affineMatrix=self.affine,
+                                              saveDir=self.dataDirectory)
+        loadedBrainMaskNifti = nib.load(join(self.dataDirectory,
+                                             'brainMask.nii.gz'))
+        self.assertTrue(np.allclose(brainMask,
+                                    loadedBrainMaskNifti.get_data()))
+        self.assertTrue(np.allclose(loadedBrainMaskNifti.affine, self.affine))
 
 
 def main():
